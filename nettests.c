fb948226305682f596e6c4a060beec62bd16046e
Rodolfo Giometti <giometti@enneenne.com>
2022-11-15 14:14:17 +0100

    nettests.c: fix up downtime message
    Signed-off-by: Rodolfo Giometti <giometti@enneenne.com>

--- nettests.c
+++ nettests.c
@@ -225,7 +225,7 @@ static void mainloop(int s, struct comm_info_s *comm)
 				info("packet out of order (last=%d curr=%d)",
 					prev_pkt_num, pkt_recv.pkt_num);
 			else if (pkt_recv.pkt_num != prev_pkt_num + 1) {
-				info("%d packets missed (downtime=%luus\n",
+				info("%d packets missed (downtime=%luus)\n",
 				     abs(pkt_recv.pkt_num - prev_pkt_num),
 				     elapsed_us);
 
